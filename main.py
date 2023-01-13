30373a48e6fab8e19cc49d934934441dd175cdcc
abhishekpeddada <abhishekpeddada@gmail.com>
2023-01-13 11:20:34 +0530

    Added screen recording option

--- main.py
+++ main.py
@@ -320,7 +320,24 @@ def screenshot():
         img.save("C:\\Saved Pictures\\ss.png")#give directory of your own
     else:
         os.mkdir(ss_dir)
-
+def record_screen():
+    resolution = (1920, 1080)
+    codec = cv2.VideoWriter_fourcc(*"XVID")
+    filename = "Recording.avi"
+    fps = 40.0
+    out = cv2.VideoWriter(filename, codec, fps, resolution)
+    cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Live", 480, 270)
+    while True:
+        img = pyautogui.screenshot()
+        frame = np.array(img)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        out.write(frame)
+        cv2.imshow('Live', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    out.release()
+    cv2.destroyAllWindows()
 def cpu():
     usage = str(psutil.cpu_percent())
     speak("CPU is at " + usage)
@@ -447,6 +464,8 @@ if __name__ == "__main__":
             print(result)
             speak(result)
             counter = 0
+        elif "record screen" in query:
+            record_screen()
         elif 'type text' in query:
             speak('what shoud i type?')
             cmd = takeCommand()
